e63c74cdf046ec13f653115c4024e81e23c1c28f
DulanKavindu <gunasekarakgdk@gmail.com>
2026-02-02 15:52:02 +0530

    corection

--- controlers/oder.js
+++ controlers/oder.js
@@ -101,12 +101,12 @@ export async function getQuter(req, res) {
       } else {
         productArray[i] = {
           name: productData.productname,
-          price: productData.lasprice,
+          price: productData.lasttprice,
           quantity: orderDetails[i].quantity,
           image: productData.image[0]
         };
 
-        total += productData.lasprice * orderDetails[i].quantity;
+        total += productData.lastprice * orderDetails[i].quantity;
         labaleTotal += productData.price * orderDetails[i].quantity;
       }
     }
