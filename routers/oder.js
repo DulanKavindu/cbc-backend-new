b4a27ce36ac3345bfb0004d29170d06f42da925d
DulanKavindu <gunasekarakgdk@gmail.com>
2026-02-03 10:45:21 +0530

    router change in order

--- routers/oder.js
+++ routers/oder.js
@@ -2,7 +2,7 @@ import express from "express";
 import { createOrder, getOrders, getQuter } from "../controlers/oder.js";
 
 const oderRouter = express.Router();    
-oderRouter.get("/",createOrder)
-oderRouter.post("/",getOrders)
+oderRouter.post("/",createOrder)
+oderRouter.get("/",getOrders)
 oderRouter.post("/getquoter",getQuter)
 export default oderRouter;
